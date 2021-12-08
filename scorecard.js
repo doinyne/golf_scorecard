a368bd3f68a762ff45cc159b7c6d6d6ee5e265bd
dean <d.lewis.rave@gmail.com>
2021-12-08 00:48:33 +0000

    fixed typo

--- scorecard.js
+++ scorecard.js
@@ -11,10 +11,10 @@ function golfScore(par, strokes) {
   else if (strokes === par) {
     return "Par";
   }
-  else if (stroke === (par + 1)) {
+  else if (strokes === (par + 1)) {
     return "Bogey";
   }
-  else if (stroke === (par + 2)) {
+  else if (strokes === (par + 2)) {
     return "Double Bogey";
   }
   else if (strokes >= (par + 3))
